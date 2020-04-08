b2cbe56641ef84fe6e5089b56d653774c83749bb
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-08 08:07:57 +0000

    Deploy gingging0822/korean-manage to github.com/gingging0822/korean-manage.git:gh-pages

---
+++ precache-manifest.5127fae36c6068ee13d34a1fe60eb75d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd48e125a7e1f117bbf3",
+    "url": "css/app.b06da546.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "3778193df520eb63f378ffea91c4ba30",
+    "url": "index.html"
+  },
+  {
+    "revision": "988fb6d26a80b1225ff0",
+    "url": "js/about.50137451.js"
+  },
+  {
+    "revision": "cd48e125a7e1f117bbf3",
+    "url": "js/app.944b66bd.js"
+  },
+  {
+    "revision": "09e81d04e3907caf12ee",
+    "url": "js/chunk-vendors.63bfe683.js"
+  },
+  {
+    "revision": "4f5e48c6bd17f0288ceab377b0ba83f8",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
